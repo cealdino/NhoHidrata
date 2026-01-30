b03fd2436feb2b70d56b8dc4f219ee639c9a8765
cealdino <cealdinoreis@gmail.com>
2026-01-30 19:02:26 -0100

    Tela quantidade de agua ingerida adicionada

--- app/src/main/java/com/example/nhohidrata/Tela_Abertura.java
+++ app/src/main/java/com/example/nhohidrata/Tela_Abertura.java
@@ -15,7 +15,7 @@ public class Tela_Abertura extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_tela_abertura);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.ingestAgua), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
